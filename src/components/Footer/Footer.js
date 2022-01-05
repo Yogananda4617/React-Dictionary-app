2bcb42154650f8834882e7f478d3394a99233799
Yogananda Potnuru <yoganandapotnuru@gmail.com>
2022-01-05 17:04:50 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <hr style={{ width: "90%", marginTop: 20 }} />
+      <span className="name">
+        Made by   Yogananda...
+        
+      </span>
+      
+    </div>
+  );
+};
+
+export default Footer;
